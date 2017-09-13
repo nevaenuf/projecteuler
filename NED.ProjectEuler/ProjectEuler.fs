fc7f03567028b5e0f4a020bc9aefc0430d3e3478
nevaenuf <amonra_mackie@hotmail.com>
2017-09-13 07:37:36 -0500

    problem 5 comments

--- NED.ProjectEuler/ProjectEuler.fs
+++ NED.ProjectEuler/ProjectEuler.fs
@@ -9,3 +9,21 @@ let main argv =
     Problem3.solve
     Problem4.solve    
     0 // return an integer exit code
+
+(*
+    Problem 5 work...
+ 2, 2, 2, 2, 5, 19, 3, 3, 17, 7, 13, 11 = 232792560
+ 
+ foreach number
+  get all prime factors
+    group prime factors by factor, count
+    
+    foreach group
+      if(list of factors contains factor)
+        if (count of factor in list < count of group)
+          add factor to count
+      else add count factors
+
+multiply all added factors together
+
+*)
